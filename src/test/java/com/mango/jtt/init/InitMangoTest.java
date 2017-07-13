aab89852781dcc9ce0a62f12fff2fa91de4456df
honghailiang <honghailiang666@126.com>
2017-07-13 16:53:31 +0800

    spring test

---
+++ src/test/java/com/mango/jtt/init/InitMangoTest.java
@@ -0,0 +1,60 @@
+package com.mango.jtt.init;
+
+import com.mango.jtt.po.Product;
+import com.mango.jtt.service.IProductService;
+import com.mango.jtt.service.IUserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * InitMango Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>ÆßÔÂ 13, 2017</pre>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath*:applicationContext*.xml")
+@ActiveProfiles("dev")
+public class InitMangoTest {
+
+    @Autowired
+    private IProductService productService;
+    @Autowired
+    private IUserService userService;
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: init()
+     */
+    @Test
+    public void testInit() throws Exception {
+        assertNotNull(productService);
+        assertNotNull(userService);
+
+        List<Product> products = productService.getProductList();
+        for (Product product : products){
+            assertEquals("Mango", product.getProductName());
+        }
+    }
+
+
+} 
